fca466f89e706acd3be72b632f3218d1d74a6eeb
Lord_Barbanuel <56373024+Emmanuel-Garnica@users.noreply.github.com>
2020-12-17 23:47:41 -0500

    Nuevo repositorio

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi import HTTPException
+from db.user_db import UserInDB
+from db.user_db import get_user,update_user
+from models.user_models import UserIn,UserOut,User
+
+api =FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080","http://localhost:8080/perfil",
+    "https://cinsell-app.herokuapp.com/"
+]
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
+@api.post("/login/")
+async def users_post(user_in:UserIn):
+    user = get_user(user_in.username)
+    if user == None:
+        raise HTTPException(status_code=404,
+    detail="El usuario no existe")
+    if user.password != user_in.password:
+            return {"Autenticado": False}
+    return {"Autenticado": True}
+
+# @api.get("/users/")
+# async def users_get(user_in:User):
+#     user = get_user(user_in.username)
+#     if user == None:
+#         raise HTTPException(status_code=404,
+#     detail="El usuario no existe")
+#     user_out = UserOut(**user.dict())
+#     return user_out
+
+@api.get("/users/{user}")
+async def users_get(user:str):
+    user = get_user(user)
+    if user == None:
+        raise HTTPException(status_code=404,
+    detail="El usuario no existe")
+    user_out = UserOut(**user.dict())
+    return user_out
